a3e9a705ea72dede3b38bb9c28c696c7368010c7
Ameen001 <81118905+AdilAmeen00@users.noreply.github.com>
2023-08-23 12:13:04 +0530

    Update privateGPT.py

--- privateGPT.py
+++ privateGPT.py
@@ -53,7 +53,7 @@ def run_model(query):
 
     # Initialize model configuration and model
     hf_token = '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'
-    model_config = transformers.AutoConfig.from_pretrained(model_id, use_auth_token=hf_token)
+    model_config = transformers.AutoConfig.from_pretrained(model_id, use_auth_token=True, use_auth_token=hf_token)
     model = transformers.AutoModelForCausalLM.from_pretrained(
         model_id,
         trust_remote_code=True,
